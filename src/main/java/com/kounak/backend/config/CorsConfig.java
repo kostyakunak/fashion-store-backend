3c2f1ecc46063e773b08b9135f7e36dbc4c5e29c
kostyakunak <koc9ihbi4@gmail.com>
2025-10-27 13:50:56 +0100

    fix: Add kounak.netlify.app to CORS allowed origins

--- src/main/java/com/kounak/backend/config/CorsConfig.java
+++ src/main/java/com/kounak/backend/config/CorsConfig.java
@@ -10,7 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000", "https://kounak2.netlify.app")
+                .allowedOriginPatterns("http://localhost:3000", 
+                                       "https://kounak.netlify.app",
+                                       "https://kounak2.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
